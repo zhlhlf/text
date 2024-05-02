fc302cbcebc8e35b93cceddc1947e09d3d70e419
zhlhlf <60917750+zhlhlf@users.noreply.github.com>
2024-05-02 23:52:03 +0800

    Update coloros_14.sh

--- sys-del_project/coloros_14.sh
+++ sys-del_project/coloros_14.sh
@@ -1,9 +1,9 @@
 de(){
  oo=$(find */*app*/* -iname $1  -maxdepth 0)
- if [ ! $oo ];then
+ if [ "$oo" == "" ];then
    oo=$(find */*/*app*/* -iname $1  -maxdepth 0)
  fi
- if [ "$oo" ];then
+ if [ "$oo" != "" ];then
   echo "删除--- $oo \"$2\"" >> ../../../del_app-by-zhlhlf.txt
   echo "删除--- $oo \"$2\""
   rm -rf $oo
