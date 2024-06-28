7f7ee0ee1f912bc042d03d124cd96d9f88fdf41b
zhlhlf <60917750+zhlhlf@users.noreply.github.com>
2024-06-28 13:48:13 +0800

    Update onedrive_mount.sh

--- onedrive_mount.sh
+++ onedrive_mount.sh
@@ -1,19 +1,21 @@
-#!/sbin/sh
+#!/bin/sh
 
 export RCLONE_CONFIG_PASS=$1
 rm -rf rclone.conf
-wget -q https://raw.github.com/zhlhlf/text/main/rclone.conf > /dev/null
-curl -s https://rclone.org/install.sh | sudo bash > /dev/null
+wget -q https://raw.github.com/zhlhlf/text/main/rclone.conf
+curl -s https://rclone.org/install.sh | sudo bash > /dev/null 2>&1
 rm -rf /home/runner/.config/rclone/
-mkdir /home/runner/.config/rclone/
-mv rclone.conf /home/runner/.config/rclone/rclone.conf
-umount zhlhlf > /dev/null
+mkdir -p /home/runner/.config/rclone/
+mv -f rclone.conf /home/runner/.config/rclone/rclone.conf
+umount zhlhlf > /dev/null 2>&1
 rm -rf zhlhlf
 mkdir zhlhlf 
-rclone mount onedrive:/$2 ./zhlhlf --umask 000 --daemon
-echo "\n"
+rclone mount onedrive:/$2 ./zhlhlf --umask 000 --daemon 
+echo "==============list==============="
 du -h 666/*
-echo "\n"
+echo
 rclone copy 666 zhlhlf/ 
-echo "\n"
+echo
+echo
+echo "==============onedrive-list==============="
 ls zhlhlf
