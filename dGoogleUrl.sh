5c2f70fd391db632ed0147d632a53c4a44d2c09f
zhlhlf <60917750+zhlhlf@users.noreply.github.com>
2024-11-23 15:50:06 +0800

    Update dGoogleUrl.sh

--- dGoogleUrl.sh
+++ dGoogleUrl.sh
@@ -1,31 +1,32 @@
-a1="https://drive.usercontent.google.com/download?id=14S0v0GMzWcOZJKNXoMOF_vt_2p9MQNvW&export=download&authuser=0"
-#xmlData=`curl "$a1"`
-xmlData=`curl "$1"`
-ff="https://drive.usercontent.google.com/download?"
-add(){
-    if [ "$1" = "" ];then
-        echo $ff
-        exit 0
-    fi
-#        echo $1
-    ff+=$1
-}
-getValue(){ 
-  i=0
-   
-  h1=`echo $xmlData | sed s/'input type="hidden"'/"\nzhlhlf"/g | grep "zhlhlf name" | awk -F"form" '{print $1}'`
-while true
-do
-  i=$((i+2))
-  g=`echo $h1 | awk -F'"' "{print $"$i"}"`
-  add $g
-  add "="
-  i=$((i+2))
-  g=`echo $h1 | awk -F'"' "{print $"$i"}"`
-  add $g
-  add "&" 
+#!/bin/sh
+
+#google url download
+# in https://drive.usercontent.google.com/download?id=18beYZJsFLoroun3DlaaI188KWdjU1Zr9&export=download&authuser=0
+
+file=data
+
+url="$1"
+
+curl "$url" > $file
+
+sed -i 's/.*<form//g' $file
+sed -i 's/<input/\n/g' $file
+
+url=`grep "action=" $file | sed "s/.*action=\"//g" | sed 's/".*//g'`
+
+sed -i "s/.*action=.*//g" $file
+sed -i "s/.*submit.*//g" $file
+sed -i "s/>/\n/g" $file
+
+sed -i "s/<.*//g" $file
+
+ff="?"
+while true;do
+    key=`grep "name" $file | head -n1 | cut -d'"' -f 4`
+    if [ ! "$key" ];then break; fi
+    value=`grep "name" $file | head -n1 | cut -d'"' -f 6`
+    ff="$ff$key=$value&"
+    sed -i "s/.*name=\"$key\".*//g" $file
 done
-  
-}
-getValue
-echo $ff
+
+echo ${url}${ff}
