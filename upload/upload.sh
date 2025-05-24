de098b78b19ede3ca6386c6fa4591146e69d13de
Your Name <you@example.com>
2025-05-24 21:01:03 +0800

    as

--- upload/upload.sh
+++ upload/upload.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-
-#1 key
+#by zhlhlf
+#1 alist_msg
 #2 挂载路径
 #3 需要上传的目录
 #4 暂时使用的目录 一般无需更改
@@ -9,20 +9,20 @@ a_host=`echo $1 | awk -F' ' '{print $1}'`
 a_username=`echo $1 | awk -F' ' '{print $2}'`
 a_password=`echo $1 | awk -F' ' '{print $3}'`
 
-if [ "$2" ]; then
+if [ "$2" ];then
     mount_dir=$2
 else
     mount_dir=tyy2/short_time_files
 fi
 echo "mount_dir: $mount_dir"
 
-if [ "$3" ]; then
+if [ "$3" ];then
     in_dir=$3
 else
     in_dir=666
 fi
 
-if [ "$4" ]; then
+if [ "$4" ];then
     current_dir=$4
 else
     current_dir=zhlhlf
@@ -44,60 +44,57 @@ mount_A_B() {
     echo "mount $1 succeeded"
 }
 
-command rclone >>/dev/null 2>&1 || curl -s https://rclone.org/install.sh | sudo bash >/dev/null 2>&1
+command rclone >>/dev/null 2>&1 || curl -s https://rclone.org/install.sh | sudo bash > /dev/null 2>&1
 mkdir alist && cd alist
 wget -q https://raw.githubusercontent.com/zhlhlf/text/refs/heads/main/upload/alist
 wget -q https://raw.githubusercontent.com/zhlhlf/text/refs/heads/main/upload/alist_back_restore.py
 wget -q https://raw.githubusercontent.com/zhlhlf/text/refs/heads/main/upload/rclone.conf
-
-unzip -qo alist_data.zip || (echo "pass fail" ; exit)
-kill -8 $(ps -A | grep alist | awk -F' ' '{print $1}') >/dev/null 2>&1
+kill -8 `ps -A | grep alist | awk -F' ' '{print $1}'` >/dev/null 2>&1
 chmod 777 * -R
-./alist server >a.log 2>&1 &
-pip install tabulate httpx >>null
+pip install tabulate httpx >> null
 python3 alist_back_restore.py --host $a_host --username $a_username --password $a_password
 python3 alist_back_restore.py
+./alist server > a.log 2>&1 &
 
 cd ..
-umount $current_dir >/dev/null 2>&1
+umount $current_dir > /dev/null 2>&1
 rm -rf $current_dir
-mkdir $current_dir
+mkdir $current_dir 
 chmod 777 $current_dir
 
 mount_A_B $mount_dir $current_dir
 
 count=$(nproc --all)
-asd() {
-    while true; do
+asd(){
+    while true;do
         sleep 6
         tail -n$count a.log
         echo
     done
 }
-asd &
+# asd &
 pid=$!
 
 echo "==============upload-list==============="
-echo -e "all_size: $(du -h $in_dir | awk '{print $1}') \n\n"
 du -h $in_dir/*
 echo
 echo "start sync"
 echo
-rclone copy $in_dir $current_dir -P --transfers=$count >a.log && kill -8 $pid
+rclone copy $in_dir $current_dir -P --transfers=$count --config ./alist/rclone.conf > a.log && kill -8 $pid
 echo
 echo "mount_dir: $mount_dir"
 echo
 
 echo "==============upload-list==============="
 cd $in_dir
-find -type f | sed 's#./##g' >../input_file_list.txt
+find -type f | sed 's#./##g' > ../input_file_list.txt
 cd ..
 cat input_file_list.txt
 echo
 
 echo "==============all-file-list==============="
 cd $current_dir
-find -type f | sed 's#./##g' >../current_file_list.txt
+find -type f | sed 's#./##g' > ../current_file_list.txt
 cd ..
 cat current_file_list.txt
 echo
@@ -108,9 +105,9 @@ while IFS= read -r i; do
     if ! grep -qF "$i" current_file_list.txt; then
         list+="$i\n"
     fi
-done <input_file_list.txt
+done < input_file_list.txt
 echo -e "$list"
 
 rm -f input_file_list.txt current_file_list.txt
-kill -8 $(ps -A | grep alist | awk -F' ' '{print $1}') >/dev/null 2>&1
+kill -8 `ps -A | grep alist | awk -F' ' '{print $1}'` >/dev/null 2>&1
 rm -r alist
