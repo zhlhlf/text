c3265edcb908aeb12c60e8d06f5e2a92a62369ba
unknown <zhlhlf@gmail.com>
2025-06-30 01:51:30 +0800

    asd

--- upload/get_openlist.sh
+++ upload/get_openlist.sh
@@ -2,7 +2,7 @@
 
 RELEASE_INFO=$(curl -s "https://api.github.com/repos/OpenListTeam/OpenList/releases/latest")
 
-DOWNLOAD_URL=$(echo "$RELEASE_INFO" | grep "browser_download_url" | grep "linux-amd64" | cut -d '"' -f 4)
+DOWNLOAD_URL=$(echo "$RELEASE_INFO" | grep "browser_download_url" | grep "linux-amd64.tar.gz" | cut -d '"' -f 4)
 
 if [ -z "$DOWNLOAD_URL" ]; then
     echo "未找到 linux-amd64 版本的下载链接。"
