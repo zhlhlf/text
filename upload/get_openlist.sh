e6f71af3f050462fb8141ea9adf8ce88a5f58e9d
zhlhlf <60917750+zhlhlf@users.noreply.github.com>
2025-08-28 10:44:13 +0800

    Update get_openlist.sh

--- upload/get_openlist.sh
+++ upload/get_openlist.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-RELEASE_INFO=$(curl -s "https://api.github.com/repos/zhlhlf/OpenList/releases/latest")
+RELEASE_INFO=$(curl -s "https://api.github.com/repos/zhlhlf/OpenList-private/releases/latest")
 
 DOWNLOAD_URL=$(echo "$RELEASE_INFO" | grep "browser_download_url" | grep "linux-musl-amd64" | cut -d '"' -f 4)
 
 if [ -z "$DOWNLOAD_URL" ]; then
     echo "未找到 linux-musl-amd64 版本的下载链接。使用固定下载地址"
-    DOWNLOAD_URL="https://github.com/zhlhlf/OpenList/releases/download/openlist_425fed6/OpenList-linux-musl-amd64"
+    DOWNLOAD_URL="https://github.com/zhlhlf/OpenList-private/releases/download/openlist_bcaa096/OpenList-private-linux-musl-amd64"
 fi
 
 # 删除openlist
