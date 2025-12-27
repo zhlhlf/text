f9dc1a47974e8112ea781f2e86597b9a67f8a199
zhlhlf <zhlhlf@gmail.com>
2025-12-27 15:04:40 +0800

    优化 upload.sh逻辑

--- upload/ABsync.sh
+++ upload/ABsync.sh
@@ -3,6 +3,7 @@
 #1 密码
 #2 get同步
 #3 put目录
+#4 同步线程数
 
 a_host=`echo $1 | awk -F' ' '{print $1}'`
 a_username=`echo $1 | awk -F' ' '{print $2}'`
@@ -11,15 +12,22 @@ a_password=`echo $1 | awk -F' ' '{print $3}'`
 A=$2
 B=$3
 
+if [ "$4" ];then
+    transfers=$4
+else
+    transfers=$(nproc)
+fi
+
 command rclone >>/dev/null 2>&1 || curl -s https://rclone.org/install.sh | sudo bash > /dev/null 2>&1
+pip install httpx >> null &
 mkdir openlist && cd openlist
 curl -sL https://raw.githubusercontent.com/zhlhlf/text/refs/heads/main/upload/get_openlist.sh | bash
 wget -q https://raw.githubusercontent.com/zhlhlf/text/refs/heads/main/upload/openlist_back_restore.py
 wget -q https://raw.githubusercontent.com/zhlhlf/text/refs/heads/main/upload/rclone.conf
-kill -8 `ps -A | grep openlist | awk -F' ' '{print $1}'` >/dev/null 2>&1
+pkill openlist
 chmod 777 * -R
-./openlist server > a.log 2>&1 &
-pip install httpx >> null
+wait
+./openlist server 2>&1 &
 python3 openlist_back_restore.py $a_host $a_username $a_password
 python3 openlist_back_restore.py
 
@@ -28,9 +36,9 @@ cd ..
 echo "start sync"
 echo
 if [ "$4" = "1" ];then
-    rclone move "openlist:/$A" "openlist:/$B" -P --transfers=16 --size-only --config ./openlist/rclone.conf
+    rclone move "openlist:/$A" "openlist:/$B" -P --transfers=$transfers --size-only --config ./openlist/rclone.conf
 else
-    rclone copy "openlist:/$A" "openlist:/$B" -P --transfers=16 --size-only --config ./openlist/rclone.conf
+    rclone copy "openlist:/$A" "openlist:/$B" -P --transfers=$transfers --size-only --config ./openlist/rclone.conf
 fi
 echo
 
--- upload/upload.sh
+++ upload/upload.sh
@@ -5,6 +5,11 @@
 #3 需要上传的目录
 #4 暂时使用的目录 一般无需更改
 
+
+# 1 openlist_msg
+# 2 远端openlist目录名
+# 3 本地上传目录
+
 a_host=`echo $1 | awk -F' ' '{print $1}'`
 a_username=`echo $1 | awk -F' ' '{print $2}'`
 a_password=`echo $1 | awk -F' ' '{print $3}'`
@@ -22,93 +27,38 @@ else
     in_dir=666
 fi
 
-if [ "$4" ];then
-    current_dir=$4
-else
-    current_dir=zhlhlf
-fi
+rm -rf local_file_list_sorted.txt current_file_list_sorted.txt openlist
 
-mount_A_B() {
-    echo "start mount $1 to $2"
-    i=0
-    while true; do
-        sleep 1
-        if [ "$i" == 60 ]; then
-            cat openlist/a.log
-            exit 1
-        fi
-        rclone mount openlist:/$1 ./$2 --umask 000 --daemon --config ./openlist/rclone.conf  >/dev/null 2>&1
-        if [ "$(df -h $2 | grep openlist)" ]; then break; fi
-        i=$((i + 1))
-    done
-    echo "mount $1 succeeded"
-}
-
-command rclone >>/dev/null 2>&1 || curl -s https://rclone.org/install.sh | sudo bash > /dev/null 2>&1
-mkdir openlist && cd openlist
+command rclone >>/dev/null 2>&1 || curl -s https://rclone.org/install.sh | sudo bash > /dev/null 2>&1 &
+mkdir -p openlist && cd openlist
 curl -sL https://raw.githubusercontent.com/zhlhlf/text/refs/heads/main/upload/get_openlist.sh | bash
 wget -q https://raw.githubusercontent.com/zhlhlf/text/refs/heads/main/upload/openlist_back_restore.py
 wget -q https://raw.githubusercontent.com/zhlhlf/text/refs/heads/main/upload/rclone.conf
-kill -8 `ps -A | grep openlist | awk -F' ' '{print $1}'` >/dev/null 2>&1
 chmod 777 * -R
-./openlist server > a.log 2>&1 &
+python3 openlist_back_restore.py $a_host $a_username $a_password
+wait
+./openlist server &
 pip install httpx >> null
-python3 openlist_back_restore.py --host $a_host --username $a_username --password $a_password
 python3 openlist_back_restore.py
-
 cd ..
-umount $current_dir > /dev/null 2>&1
-rm -rf $current_dir
-mkdir $current_dir 
-chmod 777 $current_dir
-
-mount_A_B $mount_dir $current_dir
-
-count=$(nproc --all)
-asd(){
-    while true;do
-        sleep 6
-        tail -n$count a.log
-        echo
-    done
-}
-# asd &
-pid=$!
 
 echo "==============upload-list==============="
 echo -e "all_size: $(du -h $in_dir | awk '{print $1}' ) \n\n"
 du -h $in_dir/*
 echo
-echo "start sync"
+echo "start sync..."
 echo
-rclone copy $in_dir $current_dir -P --transfers=16 --size-only --config ./openlist/rclone.conf > a.log && kill -8 $pid || ( cat openlist/a.log && cat a.log )
+rclone copy $in_dir openlist:$mount_dir -P --transfers=16 --size-only --config ./openlist/rclone.conf > openlist/a.log
 echo
-echo "mount_dir: $mount_dir"
-echo
-
-echo "==============upload-list==============="
-cd $in_dir
-find -type f | sed 's#./##g' > ../input_file_list.txt
-cd ..
-cat input_file_list.txt
-echo
-
-echo "==============all-file-list==============="
-cd $current_dir
-find -type f | sed 's#./##g' > ../current_file_list.txt
-cd ..
-cat current_file_list.txt
-echo
-
 echo "==============fail-list==============="
+find "$in_dir" -type f | sed "s#^$in_dir/##" | sort > local_file_list_sorted.txt
+rclone lsf openlist:/$mount_dir --files-only --recursive --config ./openlist/rclone.conf | sort > current_file_list_sorted.txt
 list=""
 while IFS= read -r i; do
-    if ! grep -qF "$i" current_file_list.txt; then
+    if ! grep -qF "$i" current_file_list_sorted.txt; then
         list+="$i\n"
     fi
-done < input_file_list.txt
+done < local_file_list_sorted.txt
 echo -e "$list"
 
-rm -f input_file_list.txt current_file_list.txt
-kill -8 `ps -A | grep openlist | awk -F' ' '{print $1}'` >/dev/null 2>&1
-rm -r openlist
+rm -rf local_file_list_sorted.txt current_file_list_sorted.txt openlist
