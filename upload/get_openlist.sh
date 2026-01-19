d74ed91d451d0cabb85da4c1a0a1dc507bf41d69
zhlhlf <zhlhlf@gmail.com>
2026-01-19 19:42:33 +0800

    优化下载自己的openlist脚本

--- upload/get_openlist.sh
+++ upload/get_openlist.sh
@@ -1,24 +1,13 @@
 #!/bin/bash
 
-RELEASE_INFO=$(curl -s "https://api.github.com/repos/zhlhlf/OpenList-private/releases/latest")
+# 2秒内访问谷歌，失败则使用代理
+timeout 2 curl -s -I "https://www.google.com" > /dev/null 2>&1 || PROXY="https://ghfast.top/"
 
-DOWNLOAD_URL=$(echo "$RELEASE_INFO" | grep "browser_download_url" | grep "linux-musl-amd64" | cut -d '"' -f 4)
+# 获取处理器架构
+ARCH=$(uname -m | sed 's/x86_64/amd64/; s/aarch64/arm64/')
 
-if [ -z "$DOWNLOAD_URL" ]; then
-    echo "未找到 linux-musl-amd64 版本的下载链接。使用固定下载地址"
-    DOWNLOAD_URL="https://github.com/zhlhlf/OpenList-private/releases/download/openlist_cc112cd/OpenList-private-linux-musl-amd64"
-fi
+URL="${PROXY}https://github.com/zhlhlf/OpenList-private/releases/download/openlist_cc112cd/OpenList-private-linux-musl-${ARCH}"
 
-# 删除openlist
 rm -rf openlist
-
-# 下载文件
-wget -q "$DOWNLOAD_URL" -O openlist
-
-chmod 777 openlist
-
-if [ $? -ne 0 ]; then
-    echo "下载文件失败。"
-    exit 1
-fi
+wget -q "$URL" -O openlist && chmod 777 openlist || echo "下载失败，请检查网络或稍后重试！"
 
