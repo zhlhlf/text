b8ae8323a226b7f942da064b2bbd18ce19289909
zhlhlf <zhlhlf@gmail.com>
2026-01-20 18:04:09 +0800

    优化下载自己的openlist脚本

--- upload/get_openlist.sh
+++ upload/get_openlist.sh
@@ -6,7 +6,7 @@ timeout 2 curl -s -I "https://www.google.com" > /dev/null 2>&1 || (PROXY="https:
 # 获取处理器架构
 ARCH=$(uname -m | sed 's/x86_64/amd64/; s/aarch64/arm64/')
 
-URL="${PROXY}https://github.com/zhlhlf/OpenList-private/releases/download/openlist_cc112cd/OpenList-private-linux-musl-${ARCH}"
+URL="${PROXY}https://github.com/zhlhlf/OpenList-private/releases/latest/download/OpenList-private-linux-musl-${ARCH}"
 
 rm -rf openlist
 wget -q "$URL" -O openlist && chmod 777 openlist || echo "下载失败，请检查网络或稍后重试！"
